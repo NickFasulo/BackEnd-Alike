37455c1d4cbc97d487b54fc3edd16c49696a0ff4
Vasilis Myrianthopoulos <vasilissubs89@gmail.com>
2023-02-09 14:46:40 -0500

    Comments on code

--- backend_alike_app/serializers.py
+++ backend_alike_app/serializers.py
@@ -1,16 +1,21 @@
+# Import Modules
 from rest_framework import serializers
+# Import Models
 from .models import User, Post, Comment
 
+# User Serializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
 
+# Post Serializer
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
 
+# Comment Serializer
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
