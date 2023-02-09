31c76c4f0aebba4bcefc71edf8a3690f77768b57
Nicholas Fasulo <fasulo.nick@gmail.com>
2023-02-09 14:01:21 -0500

    working on models

--- backend_alike_app/models.py
+++ backend_alike_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(
+      max_length = 30,
+      unique = True
+      )
+    email = models.EmailField(
+      max_length = 40,
+      unique = True
+      )
+
+    def __str__(self):
+        return f'{self.username}'
+
+class Post(models.Model):
+    image = models.URLField()
+    project_name = models.CharField(max_length = 30)
+    github_link = models.URLField()
+    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return f'{self.username}'
+
