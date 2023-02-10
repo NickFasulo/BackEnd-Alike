654886d5783397be15140bb5517c598311f6e604
Nicholas Fasulo <fasulo.nick@gmail.com>
2023-02-10 11:56:46 -0500

    add railway link to allowed hosts

--- backend_alike/settings.py
+++ backend_alike/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-eu4c7i=x5k_%-w_@qh&ncswo=8i4e%0i3m8bw-qtzn4j-@4zo!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'backend-alike.herokuapp.com', 'web-production-fdd8.up.railway.app']
 
 
 # Application definition
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': str(os.getenv('NAME')),
         'USER': str(os.getenv('USER')),
         'PASSWORD': str(os.getenv('PASSWORD')),
-        'HOST': str(os.getenv('HOST')),
+        'HOST': ALLOWED_HOSTS,
         'PORT': str(os.getenv('PORT'))
     }
 }
@@ -131,3 +131,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES[‘default’].update(db_from_env)
