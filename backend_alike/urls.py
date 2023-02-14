88b6b3e82f7333a9e73ccb36c4f62e1352e93ecb
Vasilis Myrianthopoulos <vasilissubs89@gmail.com>
2023-02-14 14:38:15 -0500

    Backend Auth

--- backend_alike/urls.py
+++ backend_alike/urls.py
@@ -17,8 +17,8 @@ router.register(r'comment', CommentViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api/token/',
+    path('/api/token/',
         TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', 
-        TokenRefreshView.as_view(), name='token_refresh')
+        TokenRefreshView.as_view(), name='token_refresh') 
 ]
