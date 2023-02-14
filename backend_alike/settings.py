b2fda3b9aaa3a473abd639d5021070a127bbcf17
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2023-02-14 13:39:08 -0500

    deleted wrong DEBUG tru/false comment

--- backend_alike/settings.py
+++ backend_alike/settings.py
@@ -26,7 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # ALLOWED_HOSTS = ['localhost','1.27.0.0.1','backend-alike.herokuapp.com']
