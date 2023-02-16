f7319f39020d3a46f3cf1574dba0bae21d2c0614
Nicholas Fasulo <fasulo.nick@gmail.com>
2023-02-16 16:15:30 -0500

    update models

--- backend_alike/settings.py
+++ backend_alike/settings.py
@@ -32,8 +32,7 @@ DEBUG = True
 # ALLOWED_HOSTS = ['localhost','1.27.0.0.1','backend-alike.herokuapp.com']
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', 'https://backend-alike.herokuapp.com', 'https://alike-ga.netlify.app', 'https://alike-ga.netlify.app/profile', 'https://alike-ga.netlify.app/home']
-
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', 'https://backend-alike.herokuapp.com', 'https://alike-ga.netlify.app',]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
@@ -41,9 +40,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
     "https://backend-alike.herokuapp.com", 
-    'https://alike-ga.netlify.app', 
-    'https://alike-ga.netlify.app/profile', 
-    'https://alike-ga.netlify.app/home'
+    'https://alike-ga.netlify.app',
 ]
 
 CORS_ALLOW_METHODS = [
--- backend_alike_app/admin.py
+++ backend_alike_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import User, Post, Comment
+from .models import UserProfile, Post, Comment
 # Register your models here.
-admin.site.register(User)
+admin.site.register(UserProfile)
 admin.site.register(Post)
 admin.site.register(Comment)
