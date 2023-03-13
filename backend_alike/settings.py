7e528010834dec105e3cfe0b241af8b36090125c
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2023-03-13 16:13:49 -0400

    changed debug to false

--- backend_alike/settings.py
+++ backend_alike/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # please don't touch
-DEBUG = True
+DEBUG = False
 
 # Allows all hosts / browsers
 ALLOWED_HOSTS = ['*']
